19b85dc9d9ad4e9cfb8b704c650ae6d5b80732aa
Seva5959 <vsevolodgannik@gmail.com>
2024-07-28 19:12:31 +0500

    last commit 0.1.1 Добавил все директории в репозиторий

---
+++ blocks.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#blocks.py
+import pygame
+from pygame import *
+import os
+import pyganim
+from pygame import transform
+
+WALL_WIDTH = 32
+WALL_HEIGHT = 32
+PLATFORM_WIDTH = 64
+PLATFORM_HEIGHT = 32
+TELEPORT_WIDTH = 32
+TELEPORT_HEIGHT = 64
+PLATFORM_COLOR = "#000000"
+ICON_DIR = os.path.dirname(__file__) #  Полный путь к каталогу с файлами
+
+class Platform(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = image.load("%s/blocks/platform.png" % ICON_DIR)
+        self.image = transform.scale(self.image, (PLATFORM_WIDTH, PLATFORM_HEIGHT))
+        self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+class BlockDie(Platform):
+    def __init__(self, x, y):
+        Platform.__init__(self, x, y)
+        self.image = image.load("%s/blocks/dieBlock.png" % ICON_DIR)
+
+class Spring(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = image.load("%s/blocks/spring.png" % ICON_DIR)
+        self.image = transform.scale(self.image, (WALL_WIDTH, WALL_HEIGHT))
+        self.rect = Rect(x, y, WALL_WIDTH, WALL_HEIGHT)
+        self.boost_power = 30  # Сила подброса
+
+    def boost(self, player):
+        player.yvel = -self.boost_power
+class Wall(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = image.load("%s/blocks/wall.png" % ICON_DIR)
+        self.image = transform.scale(self.image, (WALL_WIDTH, WALL_HEIGHT))
+        self.rect = Rect(x, y, WALL_WIDTH, WALL_HEIGHT)
+
+class BlockTree(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = image.load("%s/blocks/block_tree.png" % ICON_DIR)
+        self.image = transform.scale(self.image, (32, 32))  # Изменяем размер на 32x32
+        self.rect = Rect(x, y, 32, 32)  # Также изменяем размер прямоугольника
+class InvisibleDeadlyBlock(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((32, 32))
+        self.image.set_alpha(0)  # Делаем блок полностью прозрачным
+        self.rect = Rect(x, y, 32, 32)
+
+class InvisibleBlock(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((32, 32))
+        self.image.set_alpha(0)  # Делаем блок полностью прозрачным
+        self.rect = Rect(x, y, 32, 32)
+class LavaBlock(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = image.load("%s/blocks/lava_block.png" % ICON_DIR)
+        self.image = transform.scale(self.image, (32, 32))  # Используем размер 32x32
+        self.rect = Rect(x, y, 32, 32)
+
+class CloudBlock(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = image.load("%s/blocks/Cloud_block.png" % ICON_DIR)
+        self.image = transform.scale(self.image, (96, 64))  # Используем размер 64x96
+        self.rect = Rect(x, y, 96, 64)
+
+class CloudSpringBlock(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = image.load("%s/blocks/Cloud_block_2.png" % ICON_DIR)
+        self.image = transform.scale(self.image, (96, 64))  # Используем размер 96x64
+        self.rect = Rect(x, y, 96, 64)
+        self.boost_power = 35  # Сила подброса (можно настроить)
+
+    def boost(self, player):
+        player.yvel = -self.boost_power
+class Platform(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = image.load("%s/blocks/platform.png" % ICON_DIR)
+        self.image = transform.scale(self.image, (PLATFORM_WIDTH, PLATFORM_HEIGHT))
+        self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+        
+class BlockTeleport(sprite.Sprite):
+    def __init__(self, x, y, teleport_id):
+        sprite.Sprite.__init__(self)
+        self.destination = None
+        self.teleport_id = teleport_id
+        self.image = image.load("%s/blocks/portal.png" % ICON_DIR)
+        self.image = transform.scale(self.image, (TELEPORT_WIDTH, TELEPORT_HEIGHT))
+        self.rect = Rect(x, y, TELEPORT_WIDTH, TELEPORT_HEIGHT)
+
+    def set_destination(self, dest):
+        self.destination = dest
+
+    def teleport(self, player):
+        if self.destination:
+            print(f"Телепортация игрока с {player.rect.x}, {player.rect.y} на {self.destination.rect.x}, {self.destination.rect.y}")
+            player.rect.x = self.destination.rect.x
+            player.rect.y = self.destination.rect.y
+            # Устанавливаем новый чекпоинт
+            player.checkpoint = self.destination
+        else:
+            print("Ошибка: назначение телепорта не установлено")
+        
+class Letter(pygame.sprite.Sprite):
+    def __init__(self, x, y, letter_number):
+        pygame.sprite.Sprite.__init__(self)
+        self.letter_number = letter_number
+        self.info_image = self.get_number_image()
+        original_image = pygame.image.load(os.path.join(ICON_DIR, "blocks", "letter.jpg"))
+        self.image = pygame.transform.scale(original_image, (52, 52))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.collected = False
+
+    def get_number_image(self):
+        image_path = os.path.join(ICON_DIR, "blocks", f"{self.letter_number}_number.jpg")
+        try:
+            original_image = pygame.image.load(image_path)
+            return pygame.transform.scale(original_image, (256, 256))
+        except FileNotFoundError:
+            print(f"Предупреждение: Файл изображения номера {self.letter_number} не найден. Использую заполнитель.")
+            placeholder = pygame.Surface((256, 256))
+            placeholder.fill((200, 200, 200))  # Серый квадрат как заполнитель
+            return placeholder
+
+    def update(self):
+        if self.collected:
+            self.image = pygame.Surface((1, 1))
+            self.image.set_alpha(0)
---
+++ player.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#player.py
+from pygame import *
+import pyganim
+import os
+import blocks
+import monsters
+from platformerhabrahabr import Menu
+
+MOVE_SPEED = 13
+MOVE_EXTRA_SPEED = 2 # ускорение
+WIDTH = 22
+HEIGHT = 32
+COLOR =  "#888888"
+JUMP_POWER = 20
+JUMP_EXTRA_POWER = 1  # дополнительная сила прыжка
+GRAVITY = 2 # Сила, которая будет тянуть нас вниз
+ANIMATION_DELAY = 0.07 # скорость смены кадров
+ANIMATION_SUPER_SPEED_DELAY = 0.04 # скорость смены кадров при ускорении
+ICON_DIR = os.path.dirname(__file__) #  Полный путь к каталогу с файлами
+
+ANIMATION_RIGHT = [('%s/mario/r1.png' % ICON_DIR),
+            ('%s/mario/r2.png' % ICON_DIR),
+            ('%s/mario/r3.png' % ICON_DIR),
+            ('%s/mario/r4.png' % ICON_DIR),
+            ('%s/mario/r5.png' % ICON_DIR)]
+ANIMATION_LEFT = [('%s/mario/l1.png' % ICON_DIR),
+            ('%s/mario/l2.png' % ICON_DIR),
+            ('%s/mario/l3.png' % ICON_DIR),
+            ('%s/mario/l4.png' % ICON_DIR),
+            ('%s/mario/l5.png' % ICON_DIR)]
+ANIMATION_JUMP_LEFT = [('%s/mario/jl.png' % ICON_DIR, 0.1)]
+ANIMATION_JUMP_RIGHT = [('%s/mario/jr.png' % ICON_DIR, 0.1)]
+ANIMATION_JUMP = [('%s/mario/j.png' % ICON_DIR, 0.1)]
+ANIMATION_STAY = [('%s/mario/0.png' % ICON_DIR, 0.1)]
+
+class Player(sprite.Sprite):
+    def __init__(self, x, y,menu):
+        sprite.Sprite.__init__(self)
+        self.menu = menu
+        self.xvel = 0   #скорость перемещения. 0 - стоять на месте
+        self.startX = x # Начальная позиция Х, пригодится когда будем переигрывать уровень
+        self.startY = y
+        self.yvel = 0 # скорость вертикального перемещения
+        self.onGround = False # На земле ли я?
+        self.image = Surface((WIDTH,HEIGHT))
+        self.image.fill(Color(COLOR))
+        self.rect = Rect(x, y, WIDTH, HEIGHT) # прямоугольный объект
+        self.image.set_colorkey(Color(COLOR)) # делаем фон прозрачным
+#        Анимация движения вправо
+        boltAnim = []
+        boltAnimSuperSpeed = []
+        self.checkpoint = None
+        for anim in ANIMATION_RIGHT:
+            boltAnim.append((anim, ANIMATION_DELAY))
+            boltAnimSuperSpeed.append((anim, ANIMATION_SUPER_SPEED_DELAY))
+        self.boltAnimRight = pyganim.PygAnimation(boltAnim)
+        self.boltAnimRight.play()
+        self.boltAnimRightSuperSpeed = pyganim.PygAnimation(boltAnimSuperSpeed)
+        self.boltAnimRightSuperSpeed.play()
+#        Анимация движения влево        
+        boltAnim = []
+        boltAnimSuperSpeed = [] 
+        for anim in ANIMATION_LEFT:
+            boltAnim.append((anim, ANIMATION_DELAY))
+            boltAnimSuperSpeed.append((anim, ANIMATION_SUPER_SPEED_DELAY))
+        self.boltAnimLeft = pyganim.PygAnimation(boltAnim)
+        self.boltAnimLeft.play()
+        self.boltAnimLeftSuperSpeed = pyganim.PygAnimation(boltAnimSuperSpeed)
+        self.boltAnimLeftSuperSpeed.play()
+        
+        self.boltAnimStay = pyganim.PygAnimation(ANIMATION_STAY)
+        self.boltAnimStay.play()
+        self.boltAnimStay.blit(self.image, (0, 0)) # По-умолчанию, стоим
+        
+        self.boltAnimJumpLeft= pyganim.PygAnimation(ANIMATION_JUMP_LEFT)
+        self.boltAnimJumpLeft.play()
+        
+        self.boltAnimJumpRight= pyganim.PygAnimation(ANIMATION_JUMP_RIGHT)
+        self.boltAnimJumpRight.play()
+        
+        self.boltAnimJump= pyganim.PygAnimation(ANIMATION_JUMP)
+        self.boltAnimJump.play()
+        self.winner = False
+
+    def update(self, left, right, up, running, platforms):
+        if up:
+            if self.onGround:  # прыгаем, только когда можем оттолкнуться от земли
+                self.yvel = -JUMP_POWER
+                if running and (left or right):  # если есть ускорение и мы движемся
+                    self.yvel -= JUMP_EXTRA_POWER  # то прыгаем выше
+                self.image.fill(Color(COLOR))
+                self.boltAnimJump.blit(self.image, (0, 0))
+
+        if left:
+            self.xvel = -MOVE_SPEED  # Лево = x- n
+            self.image.fill(Color(COLOR))
+            if running:  # если ускорение
+                self.xvel -= MOVE_EXTRA_SPEED  # то передвигаемся быстрее
+                if not up:  # и если не прыгаем
+                    self.boltAnimLeftSuperSpeed.blit(self.image, (0, 0))  # то отображаем быструю анимацию
+            else:  # если не бежим
+                if not up:  # и не прыгаем
+                    self.boltAnimLeft.blit(self.image, (0, 0))  # отображаем анимацию движения
+            if up:  # если же прыгаем
+                self.boltAnimJumpLeft.blit(self.image, (0, 0))  # отображаем анимацию прыжка
+
+        if right:
+            self.xvel = MOVE_SPEED  # Право = x + n
+            self.image.fill(Color(COLOR))
+            if running:
+                self.xvel += MOVE_EXTRA_SPEED
+                if not up:
+                    self.boltAnimRightSuperSpeed.blit(self.image, (0, 0))
+            else:
+                if not up:
+                    self.boltAnimRight.blit(self.image, (0, 0))
+            if up:
+                self.boltAnimJumpRight.blit(self.image, (0, 0))
+
+        if not (left or right):  # стоим, когда нет указаний идти
+            self.xvel = 0
+            if not up:
+                self.image.fill(Color(COLOR))
+                self.boltAnimStay.blit(self.image, (0, 0))
+
+        if not self.onGround:
+            self.yvel += GRAVITY
+
+        self.onGround = False  # Мы не знаем, когда мы на земле((
+        self.rect.y += self.yvel
+        collision_result = self.collide(0, self.yvel, platforms)
+        self.collide(0, self.yvel, platforms)
+
+        self.rect.x += self.xvel  # переносим свои положение на xvel
+        self.collide(self.xvel, 0, platforms)
+
+    def collide(self, xvel, yvel, platforms):
+        for p in platforms:
+            if sprite.collide_rect(self, p):
+                if isinstance(p, blocks.BlockDie) or isinstance(p, monsters.Monster) or isinstance(p,
+                                                                                                   blocks.LavaBlock) or isinstance(
+                        p, blocks.InvisibleDeadlyBlock):
+                    self.die()
+                elif isinstance(p, blocks.BlockTeleport):
+                    print("Игрок столкнулся с телепортом")
+                    p.teleport(self)
+                elif isinstance(p, blocks.Letter):
+                    if not p.collected:
+                        p.collected = True
+                        self.menu.add_image(p.info_image)
+                        print(f"Collected letter: {p.letter_number}")
+                        return f"show_number_{p.letter_number}"
+                elif isinstance(p, blocks.Spring) or isinstance(p, blocks.CloudSpringBlock):
+                    if yvel > 0:  # Если игрок падает на пружину или облако-пружину
+                        p.boost(self)
+                elif isinstance(p, blocks.CloudBlock):
+                    if yvel > 0:  # Игрок может стоять на облаке
+                        self.rect.bottom = p.rect.top
+                        self.onGround = True
+                        self.yvel = 0
+                else:
+                    if xvel > 0:
+                        self.rect.right = p.rect.left
+                    if xvel < 0:
+                        self.rect.left = p.rect.right
+                    if yvel > 0:
+                        self.rect.bottom = p.rect.top
+                        self.onGround = True
+                        self.yvel = 0
+                    if yvel < 0:
+                        self.rect.top = p.rect.bottom
+                        self.yvel = 0
+
+        return None
+
+    def teleporting(self, goX, goY):
+        self.rect.x = goX
+        self.rect.y = goY
+
+    def die(self):
+        time.wait(500)
+        if self.checkpoint:
+            self.teleporting(self.checkpoint.rect.x, self.checkpoint.rect.y)
+        else:
+            self.teleporting(self.startX, self.startY)
